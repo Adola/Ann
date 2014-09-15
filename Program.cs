c6d19a450300b6b34f80577ad122ec6c1aaabef5
Christopher Causey <christopher_cause1@eku.edu>
2014-09-14 23:37:09 -0400

    Added a class for sending keyboard inputs.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Player
@@ -9,12 +11,23 @@ namespace Player
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
         [STAThread]
         static void Main( )
         {
+            // set Jnes as the foreground and focused on window. 
+            var prc = Process.GetProcessesByName("Jnes");
+            if (prc.Length > 0)
+                SetForegroundWindow(prc[0].MainWindowHandle);
+
+            
             Application.EnableVisualStyles( );
             Application.SetCompatibleTextRenderingDefault( false );
             Application.Run( new MainForm( ) );
+            
         }
     }
 }
