33e123dc948108f841fb7f07bf3ac7545f2cc904
Christopher Causey <christopher_cause1@eku.edu>
2014-09-18 17:47:05 -0400

    added code for taking screenshots

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
@@ -12,22 +13,52 @@ namespace Player
         /// The main entry point for the application.
         /// </summary>
         /// 
+
+        static public Bitmap screenBMP;
+        static public Graphics g;
+        static public Size s;
+        static public Rect emuRect;
+
         [DllImport("user32.dll")]
         private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+        public struct Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+        }
+
+        public static void updateScreenshot()
+        {
+            screenBMP = new Bitmap(emuRect.Right - emuRect.Left + 1, emuRect.Bottom - emuRect.Top + 1);
+            g = Graphics.FromImage(screenBMP);
+            g.CopyFromScreen(emuRect.Left, emuRect.Top, 0, 0, s);
+        }
 
         [STAThread]
         static void Main( )
         {
-            // set Jnes as the foreground and focused on window. 
-            var prc = Process.GetProcessesByName("Jnes");
-            if (prc.Length > 0)
-                SetForegroundWindow(prc[0].MainWindowHandle);
+            String EmulatorProcessName = "Jnes";
+            Process[] processes = Process.GetProcessesByName(EmulatorProcessName);
+            Process proc = processes[0];
+            IntPtr ptr = proc.MainWindowHandle;
+            emuRect = new Rect();
+            GetWindowRect(ptr, ref emuRect);
+            s = new Size(emuRect.Right-emuRect.Left + 1, emuRect.Bottom - emuRect.Top + 1);
+            SetForegroundWindow(proc.MainWindowHandle); // ! DOES NOT WORK IF EMULATOR HAS BEEN MINIMIZED !
 
-            
+            Form tc = new testcontrols();
+            tc.Visible = true;
             Application.EnableVisualStyles( );
             Application.SetCompatibleTextRenderingDefault( false );
             Application.Run( new MainForm( ) );
-            
+
         }
     }
 }
